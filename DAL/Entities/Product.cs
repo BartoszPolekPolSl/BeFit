76356eaf83c218a3dfff91409d804b6b5ef9ab13
Bartosz Polek <bartek.polek@wp.pl>
2021-06-16 17:37:25 +0200

    Added DAL

---
+++ DAL/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFit.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Proteins { get; set; }
+        public double Fats { get; set; }
+        public double Kcal { get; set; }
+        public Product(string name, double weight, double fats, double carbohydrates, double proteins) 
+        {
+            Name = name;
+            Weight = weight;
+            Carbohydrates = carbohydrates;
+            Proteins = proteins;
+            Fats = fats;
+            Kcal= Math.Round((Weight / 100) * Carbohydrates * 4 + (Weight / 100) * Fats * 9 + (Weight / 100) * Proteins * 4);
+        }
+    }
+}
---
+++ DAL/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFit.Model
+{
+    class User
+    {
+        int Id { get; set; }
+        string UserName { get; set; }
+        string Sex { get; set; }
+        double Weight { get; set; }
+        int Height { get; set; }
+        int Age { get; set; }
+        string Activity { get; set; }
+        string Target { get; set; }
+    }
+}
